6e59d33a4520fd9b5cb22ec44014da0784c7d495
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-02-01 09:45:22 -0800

    Added things to hangman
    2.1.18

--- Leslie Coleman - Hangman2.py
+++ Leslie Coleman - Hangman2.py
@@ -9,31 +9,54 @@ import string
 import sys
 alphabet = string.ascii_lowercase
 words_phrases = ['gabriella montez', 'chad danforth', 'sharpay evans', 'kelsi nielsen', 'taylor mckessie',
-                  'ms darbus', 'zeke baylor', 'troy bolton', 'ryan evans', 'coach jack bolton']
-guesses_left = 10
-regular_word = random.choice(words_phrases).lower()
-word = list(regular_word)
-guesses = [' ']
-hidden_word = ('*' * len(regular_word))
-print(hidden_word)
+                 'ms darbus', 'zeke baylor', 'troy bolton', 'ryan evans', 'coach jack bolton']
 
-while guesses_left > 0:
-    print(guesses)
-    print("The word is %s letters long." % len(regular_word))
-    player_guess = input("take a guess >_").lower()
-    guesses.append(player_guess)
-    if player_guess not in regular_word:
-        guesses_left -= 1
-    print("You now have %s guesses" % guesses_left)
-    output = []
-    for letter in regular_word:
-        if letter in guesses:
-            output.append(letter)
-        else:
-            output.append('*')
-    output = ''.join(output)
-    if '*' not in output:
-        print('Congrats you win. The word was %s' % regular_word)
-        sys.exit()
-    print(output)
-print("Sorry you didn't guess correct. The word was %s" % regular_word)
+
+def hangman():
+
+    guesses_left = 10
+    regular_word = random.choice(words_phrases).lower()
+    word = list(regular_word)
+    guesses = [' ']
+    hidden_word = ('*' * len(regular_word))
+    print(hidden_word)
+
+    while guesses_left > 0:
+        print(guesses)
+        print("Hello. Welcome to Hangman. The word is %s letters long." % len(regular_word))
+        print("If it gets difficult for you, type quit and the game will stop.")
+        player_guess = input("take a guess >_").lower()
+        guesses.append(player_guess)
+        if player_guess == 'quit':
+            print("Sorry it was hard. The word was %s. Have a good day." % regular_word)
+            again = input("Would you like to play again? (yes/no)")
+            if again == "yes":
+                hangman()
+            else:
+                exit(0)
+        if player_guess not in regular_word:
+            guesses_left -= 1
+        print("You now have %s guesses" % guesses_left)
+        output = []
+        for letter in regular_word:
+            if letter in guesses:
+                output.append(letter)
+            else:
+                output.append('*')
+        output = ''.join(output)
+        if '*' not in output:
+            print('Congrats you win. The word was %s' % regular_word)
+            again = input("Would you like to play again? (yes/no)")
+            if again == "yes":
+                hangman()
+            else:
+                exit(0)
+        print(output)
+    print("Sorry you didn't guess correct. The word was %s" % regular_word)
+    again = input("Would you like to play again? (yes/no)")
+    if again == "yes":
+        hangman()
+    else:
+        exit(0)
+
+hangman()
