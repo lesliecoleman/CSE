17467c13462bf32d2f4dbd295f2b2f3a87d847e9
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-02-15 09:45:08 -0800

    Worked on Map
    2.15.18

--- Leslie Coleman - Dictionary World Map2.py
+++ Leslie Coleman - Dictionary World Map2.py
@@ -9,8 +9,8 @@ world_map = {
     'BEDROOM': {
         'NAME': 'Empty Bedroom',
         'DESCRIPTION': 'You enter what looks like a bedroom. In the upper right corner you see a person trapped in a '
-                       'cage pointing to writing on the wall saying “Don’t Trust The Ducks!” In another corner you see '
-                       'a rubber duck wearing a fedora. There is a path to the west and a path to the east.',
+                    'cage pointing to writing on the wall saying “Don’t Trust The Ducks!” \nIn another corner you see '
+                    'a rubber duck wearing a fedora. There is a path to the west and a path to the east.',
         'PATHS': {
             'WEST': 'HOLE_1',
             'EAST': 'KITCHEN'
@@ -20,7 +20,15 @@ world_map = {
         'NAME': 'Hole',
         'DESCRIPTION': 'You fall down a deep hole and land on a matress. There is a path to the south.',
         'PATHS': {
-            'SOUTH': ''
+            'SOUTH': 'CAVE_1'
+        }
+    },
+    'CAVE_1': {
+        'NAME': 'Cave',
+        'DESCRIPTION': 'You enter a dirt cave with one torch on the floor. There is two paths to the east and north.',
+        'PATHS': {
+            'EAST': 'M_BOX',
+            'NORTH': 'HOLE_1'
         }
     },
     'KITCHEN': {
@@ -28,21 +36,33 @@ world_map = {
         'DESCRIPTION': 'You are now in the kitchen. On the table in the upper left corner there is a pocket knife. '
                        'There is four paths: West, North, South, and East.',
         'PATHS': {
-
+            'WEST': 'BEDROOM',
+            'NORTH': 'HOLE_2',
+            'SOUTH': 'S_BOX',
+            'EAST': 'BATHROOM_1'
         }
     },
     'HOLE_2': {
         'NAME': 'Hole',
-        'DESCRIPTION': 'You fall down another deep hole to your death.',
+        'DESCRIPTION': 'You fall down another deep hole and there is a path to the north-west.',
         'PATHS': {
-
+            'NORTHWEST': 'CAVE_2'
+        }
+    },
+    'CAVE_2': {
+        'NAME': 'Cave',
+        'DESCRIPTION': 'You are in another dirt cave. There is two paths to the south-west and south-east.',
+        'PATHS': {
+            'SOUTHWEST': 'BEDROOM',
+            'SOUTHEAST': 'HOLE_2'
         }
     },
     'S_BOX': {
         'NAME': 'Steel Box',
         'DESCRIPTION': 'You are now in a steel box. There is a path to the north and one to the west.',
         'PATHS': {
-
+            'NORTH': 'KITCHEN',
+            'WEST': 'M_BOX'
         }
     },
     'BATHROOM_1': {
@@ -50,7 +70,9 @@ world_map = {
         'DESCRIPTION': 'You enter the bathroom. On the shelf there is a half-eaten sandwich. There is three paths: '
                        'East, West, and South.',
         'PATHS': {
-
+            'EAST': 'GRASS',
+            'WEST': 'KITCHEN',
+            'SOUTH': 'STORAGE'
         }
     },
     'STORAGE': {
@@ -66,7 +88,8 @@ world_map = {
         'DESCRIPTION': 'You leave the bathroom and you are now in a grassy field. To the north and to the east there '
                        'is a brick wall. There are two paths to the west and south.',
         'PATHS': {
-
+            'WEST': 'BATHROOM_1',
+            'SOUTH': 'BARN'
         }
     },
     'BARN': {
@@ -74,7 +97,8 @@ world_map = {
         'DESCRIPTION': 'You walk through two open doors into a barn. There is two paths leading west and north. '
                        'It appears that nothing is in the barn but you feel like there is something watching you.',
         'PATHS': {
-
+            'NORTH': 'GRASS',
+            'WEST': 'BATHROOM_2'
         }
     },
     'BATHROOM_2': {
@@ -82,15 +106,17 @@ world_map = {
         'DESCRIPTION': 'You enter a fancy bathroom. On the wall there is a mirror that is partially broken. '
                        'There are two paths to the west and to the east.',
         'PATHS': {
-
+            'EAST': 'BARN',
+            'WEST': 'POOL'
         }
     },
     'POOL': {
         'NAME': 'Pool',
         'DESCRIPTION': 'You leave the bathroom and walk onto a pool deck. Floating in the pool is another rubber duck '
-                       'wearing a fedora. There are two paths to the west and to the north-west. ',
+                       'wearing a fedora. There are two paths to the east and to the north-west. ',
         'PATHS': {
-
+            'NORTHWEST': 'LIBRARY',
+            'EAST': 'BATHROOM_2'
         }
     },
     'LIBRARY': {
@@ -98,15 +124,17 @@ world_map = {
         'DESCRIPTION': 'You walk into a library full of dusty books. You look around and find 600 rubber ducks wearing '
                        'fedoras all over the room. You need to get out either to the west or south-east.',
         'PATHS': {
-
+            'WEST': 'CLASS',
+            'SOUTHEAST': 'POOL'
         }
     },
     'CLASS': {
         'NAME': 'Classroom',
         'DESCRIPTION': 'You walk into what look likes an old classroom. Books are left all over the desks and there is '
-                       'a staircase in the back. There are two paths leading out, down the staircase and to the east',
+                       'a staircase in the back. \nThere are two paths leading out, down the staircase and to the east',
         'PATHS': {
-
+            'EAST': 'LIBRARY',
+            'DOWN': 'STORAGE_2'
         }
     },
     'STORAGE_2': {
@@ -114,16 +142,19 @@ world_map = {
         'DESCRIPTION': 'You enter a room full of seafoam green colored whiteboard markers. There is two paths up the '
                        'staircase and down the staircase.',
         'PATHS': {
-
+            'UP': 'CLASS',
+            'DOWN': 'STORAGE_3'
         }
     },
     'STORAGE_3': {
         'NAME': 'Large Storage Room (Boss Area)',
         'DESCRIPTION': 'You enter a dim lighted storage room. You see over 3,000 tiny rubber ducks in the room '
-                       'surrounding a the largest rubber duck in the world. You see that your armor has been upgraded '
-                       'and you now have a sword to fight the ducks.',
+                    'surrounding a the largest rubber duck in the world. You see that your armor has been upgraded '
+                    'and \nyou now have a sword to fight the ducks. There is a path to the east but it is locked, and '
+                    'a path up the stairs. Get the key to unlock the door.',
         'PATHS': {
-
+            'UP': 'STORAGE_2',
+            'EAST': 'PARTY'
         }
     },
     'PARTY': {
@@ -133,11 +164,11 @@ world_map = {
         'PATHS': {
 
         }
-    }
+    },
 }
 
 current_node = world_map['M_BOX']
-directions = ['NORTH', 'SOUTH', 'WEST', 'EAST', 'NORTHWEST', 'SOUTHEAST', 'UP', 'DOWN']
+directions = ['NORTH', 'SOUTH', 'WEST', 'EAST', 'NORTHWEST', 'SOUTHEAST', 'UP', 'DOWN', 'SOUTHWEST']
 
 while True:
     print(current_node['NAME'])
