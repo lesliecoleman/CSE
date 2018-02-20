3907e71fe2df6c80785598919630c78c37939f23
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-02-20 09:46:37 -0800

    Took notes on Functions
    2.20.18

---
+++ Function Notes.py
@@ -0,0 +1,40 @@
+# Defining functions
+def hello_world():
+    print("Hello World")
+
+
+hello_world()
+
+
+def square_it(number):
+    return number**2
+
+
+print(square_it(3))
+
+
+def tip_calc(bill):
+    tax_amt = bill * 0.18
+    total_bill = bill + tax_amt
+    return total_bill
+
+
+print(tip_calc(0.50))
+
+
+def distance_calc(x1, y1, x2, y2):
+    inside = (x2 - x1) ** 2 + (y2 - y1) ** 2
+    answer = inside ** 0.5
+    return answer
+
+
+print(distance_calc(0, 0, 3, 4))
+
+
+def theorem_calc(x1, y1):
+    inside = (x1) ** 2 + (y1) ** 2
+    answer = inside ** 0.5
+    return answer
+
+
+print(theorem_calc(5, 4))
