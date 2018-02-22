3ac278150989d46832b117a15836b565d7d55207
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-02-22 09:46:10 -0800

    finshed notes. started new project
    2.22.18

--- Class Notes.py
+++ Class Notes.py
@@ -27,8 +27,10 @@ burger1.eat()
 print(burger1.eaten)
 print(burger2.eaten)
 
+# Car can drive, explode, flip, turn
 
-class car(object):
+
+class Car(object):
     def __init__(self, name, color, num_of_doors, hp):
         self.color = color
         self.doors = num_of_doors
@@ -36,3 +38,41 @@ class car(object):
         self.HP = hp
         self.passengers = 0
         self.name = name
+        self.air_conditioning = True
+
+    def turn_on(self):
+        if self.running:
+            print("Nothing Happens.")
+        else:
+            self.running = True
+            print("The car starts.")
+
+    def move_forward(self):
+        if self.running:
+            print("You move forward")
+        else:
+            print("Nothing Happens")
+
+    def turn_off(self):
+        if self.running:
+            self.running = False
+            print("You turn the car off")
+        else:
+            print("The car is already off")
+
+    def go_for_drive(self, passengers):
+        print("%d passengers get in." % passengers)
+        self.passengers = passengers
+        self.turn_on()
+        self.move_forward()
+        self.move_forward()
+        self.move_forward()
+        self.turn_off()
+        print("%d passengers get out." % passengers)
+        self.passengers = 0
+
+
+my_car = Car('Jeffery', 'Neon Yellow', 2000, 999999999999999999999199999999999)
+
+car2 = Car('Yreffej', 'Neon Blue', 4000, 999999999999999999999999999999999999999999999999999999999999999999999999999999)
+my_car.go_for_drive(777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777)
---
+++ Leslie Coleman - World Map OOP.py
@@ -0,0 +1,6 @@
+class Room(object):
+    def __init__(self, doors, sandwich, man, rubber_duck):
+        self.doors = doors
+        self.half_eaten_sandwich = sandwich
+        self.man = man
+        self.rubber_duck = rubber_duck
