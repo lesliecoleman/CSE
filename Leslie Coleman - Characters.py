7ff17142b1eef20a45eb54a5749273557ea65717
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-03-08 09:45:22 -0800

    3.8.18
    Made Civil War w/ friend
    (Worked on Characters)

--- Leslie Coleman - Characters.py
+++ Leslie Coleman - Characters.py
@@ -1,38 +1,55 @@
-'''
-Name
-Health
-Pick up items
-Move?
-Attack
-Death
-Dialogue
-Perform Actions (Use, Push, etc.)
-Description
-Status Effect (paralyze, poison, burn, etc.)
-Take Damage
-'''
+# Name
+# Health
+# Pick up items
+# Move?
+# Attack
+# Death
+# Dialogue
+# Perform Actions (Use, Push, etc.)
+# Description
+# Status Effect (paralyze, poison, burn, etc.)
+# Take Damage
+
 
 class Character(object):
-    def __init__(self, name, health, description, dialogue, holding):
+    def __init__(self, name, description, dialogue, holding):
         self.name = name
-        self.health = health
+        self.health = 6
         self.description = description
         self.dialogue = dialogue
-        self.holding = False
+        self.holding = holding
+        self.death = False
 
     def pick_up(self):
         if self.holding:
             print("Not available")
         else:
             print("You pick up the item.")
-    def attack(self):
-        print("You attack.")
-    def death(self):
-        if self.health < 0
-            self.health = True
-        else:
-            self.health = False
-    def damage(self):
-        self.damage -= 1
 
-iron_man = Character('Tony Stark', 100, "Rich man in a suit", None)
+    def attack(self, target):
+        print("%s attacks %s" % (self.name, target.name))
+        target.damage()
+
+    def death(self):
+        if self.health < 0:
+            self.death = True
+        else:
+            self.death = False
+
+    def damage(self):
+        self.health -= 1
+        print("%s has %s health" % (self.name, self.health))
+
+
+print("I present to you Civil War")
+cap_america = Character('Steve Rogers', 'Large man in a colorful suit holding a round shield.', None, None)
+iron_man = Character('Tony Stark', "Rich man in a suit. (Might also be Batman in disguise.)", None, None)
+print(cap_america.description)
+print(iron_man.description)
+iron_man.attack(cap_america)
+cap_america.attack(iron_man)
+cap_america.attack(iron_man)
+cap_america.attack(iron_man)
+cap_america.attack(iron_man)
+cap_america.attack(iron_man)
+cap_america.attack(iron_man)
