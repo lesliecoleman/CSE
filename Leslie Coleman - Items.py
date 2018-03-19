80815aa720fba79e0c143eb0e3bd10712adf0181
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-03-19 09:45:04 -0700

    Continued working on items
    3.19.18

--- Leslie Coleman - Items.py
+++ Leslie Coleman - Items.py
@@ -1,8 +1,21 @@
-class item(object):
-    def __init__(self, name, weight, value):
+class Item(object):
+    def __init__(self, name):
         self.name = name
-        self.weight = weight
-        self.value = value
 
     def take(self):
-        
+        if player_inv < 15:
+            print("You grab the %s" % self.name)
+            player_inv.append(self)
+
+        elif player_inv == 15:
+            print("You don't have space to pick up the %s" % self.name)
+
+class Food(Item):
+    def __init__(self, name, eat):
+        self.eat = eat
+        super(Food, self).__init__(name)
+
+    def eat(self):
+        if
+
+player_inv = []
