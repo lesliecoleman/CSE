c2eae3d4da08715a0b8b8f66da30b1d0b02931ee
lesliecoleman <31074327+lesliecoleman@users.noreply.github.com>
2018-03-20 09:44:56 -0700

    Continued working on items class
    3.2018

--- Leslie Coleman - Items.py
+++ Leslie Coleman - Items.py
@@ -10,12 +10,27 @@ class Item(object):
         elif player_inv == 15:
             print("You don't have space to pick up the %s" % self.name)
 
+
 class Food(Item):
     def __init__(self, name, eat):
         self.eat = eat
         super(Food, self).__init__(name)
 
     def eat(self):
-        if
+        if self.eat:
+            print("You eat the %s. Weird." % self.name)
+            player_inv.remove(self)
+        else:
+            print("You can not eat that. That's weird.")
+
+        
+class Weapons(Item):
+    def __init__(self, name, eat, attack):
+        self.attack = attack
+        super(Weapons, self).__init__(name)
+
+    def attack(self):
+        
+
 
 player_inv = []
